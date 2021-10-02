d5b872e57f3e26cfe8c04e4a45f944c0e05a9e58
Floozutter <me@floozutter.site>
2021-10-02 03:55:34 -0700

    add horse stuff
    in hindsight, jumping straight into a game jam after getting blood work
    done was maybe a bit too ambitious. but now that i've napped and regen'd
    my life juice, it's time for full steam ahead!

--- src/mainstate.rs
+++ src/mainstate.rs
@@ -1,33 +1,42 @@
+enum Direction {
+    Left,
+    Right
+}
+
 enum HorseKind {
     Brown,
     Gray,
     Gold,
     Book,
 }
-
-struct Horse {
+struct TowerHorse {
     kind: HorseKind,
-    x: f32,
-    y: f32,
+    direction: Direction,
+    x: i64,
+}
+struct DropHorse {
+    kind: HorseKind,
+    direction: Direction,
+    x: i64,
+    y: i64,
 }
 
 pub struct MainState {
-    horses: Vec<Horse>,
+    horsetower: Vec<TowerHorse>,
+    drophorse: Option<DropHorse>,
 }
-
 impl MainState {
     pub fn new(_ctx: &mut ggez::Context) -> ggez::GameResult<MainState> {
         Ok(MainState{
-            horses: Vec::new(),
+            horsetower: Vec::new(),
+            drophorse: None,
         })
     }
 }
-
 impl ggez::event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
         Ok(())
     }
-
     fn draw(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
         Ok(())
     }
